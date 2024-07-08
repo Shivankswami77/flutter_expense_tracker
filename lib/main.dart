e8d50539878af05c709097aa7c9f7ae90c7b6bf5
shivank_swami <shivank.swami@icreon.com>
2024-07-08 16:14:34 +0530

    added dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,28 @@ import 'package:expense_tracker/widgets/expenses.dart';
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 5, 99, 124),
+    brightness: Brightness.dark);
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer),
+        ),
+      ),
       theme: ThemeData().copyWith(
           useMaterial3: true,
-          scaffoldBackgroundColor: const Color.fromARGB(255, 197, 229, 244),
           colorScheme: kColorScheme,
           appBarTheme: const AppBarTheme().copyWith(
               backgroundColor: kColorScheme.onPrimaryContainer,
@@ -29,6 +45,7 @@ void main() {
                     color: kColorScheme.onSecondaryContainer,
                     fontSize: 14),
               )),
+      themeMode: ThemeMode.system,
       home: const Expenses(),
     ),
   );
